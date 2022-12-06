5f05deadedc70684b9bb3ed4855d9ed9b42649b2
Alessia B <borys.alessia@gmail.com>
2022-12-06 16:09:22 +0100

    input field

--- src/components/ToDoListFunction.tsx
+++ src/components/ToDoListFunction.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react"
 
 export interface IToDo {
     task: string;
-    // complete: boolean;
+    completed: boolean;
     id: number;
 }
 
@@ -13,6 +13,9 @@ export default function ToDoListFunction(): JSX.Element {
     const [toDoList, setToDoList] = useState<IToDo[]>([]);
     const [input, setInput] = useState<string>('')
     const [update, setUpdate] = useState<boolean>(false);
+    //const [formState, setFormState] = useState({
+    //    task: '',
+    //});
 
     // Update to-dos on start
     useEffect(() => {
@@ -27,14 +30,44 @@ export default function ToDoListFunction(): JSX.Element {
         setToDoList(response.data);
         } catch (error) {
         console.error(
-            "Woops... there's an issue with your GET request: ",
+            "Woops... issue with GET request: ",
             error
         );
         }
     };
 
+    //POST to do to API
+    const postToDoList = async (toDoTask: string) => {
+        console.log("postToDoArr function is running!");
+        try {
+          await axios.post(url + "/items", { task: toDoTask });
+        } catch (error) {
+          console.error(
+            "Woops... issue with POST request: ",
+            error
+          );
+        }
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(input)
+        postToDoList(input)
+    }
+    //PATCH a to do
+    //DELETE completed to dos
+
     return (
         <>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    name='task'
+                    type='text'
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button type="submit" ></button>
+            </form>
             {toDoList.map((el) => {
                 return (
                     <div key={el.id}>{el.task}</div>
